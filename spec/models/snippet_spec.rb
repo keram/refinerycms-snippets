6ad665334b2ae37c3b3cfc8358f710c92c5391a3
Ruben Sierra <ruben.sierra@the-cocktail.com>
2011-12-14 18:45:15 +0100

    Specs updated.
    The last snippet spec crashed, becouse parts didn't have the correct
    association with page.

--- spec/models/snippet_spec.rb
+++ spec/models/snippet_spec.rb
@@ -1,45 +1,51 @@
 require 'spec_helper'
 
-describe Snippet do
+module Refinery
+  module Snippets
+    describe Snippet do
 
-  def reset_snippet(options = {})
-    @valid_attributes = {
-      :id => 1,
-      :title => "RSpec is great for testing too"
-    }
+      def reset_snippet(options = {})
+        @valid_attributes = {
+          :id => 1,
+          :title => "RSpec is great for testing too"
+        }
 
-    @snippet.destroy! if @snippet
-    @snippet = Snippet.create!(@valid_attributes.update(options))
-  end
+        @snippet.destroy! if @snippet
+        @snippet = Snippet.create!(@valid_attributes.update(options))
+      end
 
-  before(:each) do
-    reset_snippet
-  end
+      before(:each) do
+        reset_snippet
+      end
 
-  context "validations" do
+      context "validations" do
 
-    it "rejects empty title" do
-      Snippet.new(@valid_attributes.merge(:title => "")).should_not be_valid
+        it "rejects empty title" do
+          Snippet.new(@valid_attributes.merge(:title => "")).should_not be_valid
+        end
+
+        it "rejects non unique title" do
+          # as one gets created before each spec by reset_snippet
+          Snippet.new(@valid_attributes).should_not be_valid
+        end
+
+      end
+
+      it 'should return the page it is attached' do
+        @snippet.pages.should be_empty
+        page = Page.create!(:title => 'Page')
+        2.times do
+          other_page = Page.create!(:title => 'Other Page')
+          part = other_page.parts.create(:title => 'Other part', :body => "OTHER PART BODY")
+          part.snippets << @snippet
+        end
+        
+        @snippet.pages.should have(2).pages
+        @snippet.pages.each do |p|
+          p.title.should_not == page.title
+        end
+      end
+
     end
-
-    it "rejects non unique title" do
-      # as one gets created before each spec by reset_snippet
-      Snippet.new(@valid_attributes).should_not be_valid
-    end
-
   end
-
-  it 'should return the page it is attached' do
-    @snippet.pages.should be_empty
-    page = Page.create!(:title => 'Page')
-    2.times do
-      part = PagePart.create!(:title => 'Other part', :body => "OTHER PART BODY", :page_id => Page.create!(:title => 'Other Page').id)
-      part.snippets << @snippet
-    end
-    @snippet.pages.should have(2).pages
-    @snippet.pages.each do |p|
-      p.title.should_not == page.title
-    end
-  end
-
 end
