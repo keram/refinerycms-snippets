2b33f863e99450f0adf73ad54bd4ff0e681788dd
Luis Eduardo Rodríguez Mendo <luis.mendo@the-cocktail.com>
2012-06-22 11:32:48 +0200

    Removing commented code.

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,3 @@
-# Rails.application.routes.draw do
-#   scope(:path => 'refinery', :as => 'refinery_admin', :module => 'refinery/admin') do
-#     resources :snippets do
-#       collection do
-#         post :update_positions
-#       end
-
-#       resources :snippets_page_parts do
-#         member do
-#           get 'add'
-#           get 'remove'
-#         end
-#       end
-#     end
-
-#   end
-# end
-
 Refinery::Core::Engine.routes.append do
 
   # Admin routes
